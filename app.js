01ad97817c05de1a4974069e3b1c12dad4fd016f
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-08-26 14:29:13 -0700

    commited all of the things that did not commit before

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-// import functions
-
-// reference needed DOM elements
+import { multiply } from './calculator.js';
 
 
-// set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+const multiplyNumber12 = document.getElementById('multiply-number-12');
+const multiplyNumber2 = document.getElementById('multiply-number-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyAnswer = document.getElementById('multiply-answer');
+
+multiplyButton.addEventListener('click', () => {
+    const num12 = Number(multiplyNumber12.value);
+    const num2 = Number(multiplyNumber2.value);
+    const sum = multiply(num12, num2);
+    multiplyAnswer.textContent = sum;
+});
+
--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,16 @@
-// IMPORT MODULES under test here:
-// import { add } from '../calculator.js';
+import { multiply } from '../calculator.js';
 
 const test = QUnit.test;
 
-// name your test by what it is testing
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+test('multiply two numbers', (expect) => {
+
+    const expected = 24;
+    
+    
+    const actual = multiply(12, 2);
+
+    
+   
     expect.equal(actual, expected);
 });
